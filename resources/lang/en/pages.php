90711edbba06ae8c068e41df07d9897eeec91ef4
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-09-21 16:01:35 +0200

    fixed missing translations

--- resources/lang/en/pages.php
+++ resources/lang/en/pages.php
@@ -83,10 +83,10 @@ return [
             'category' => [
                 'title' => 'Exercise categories',
             ],
+            'last_dominant_category' => 'Last dominant category',
+            'last_total_weight' => 'Last workout total weight',
+            'last_total_reps' => 'Last workout total reps',
         ],
-        'last_dominant_category' => 'Last dominant category',
-        'last_total_weight' => 'Last workout total weight',
-        'last_total_reps' => 'Last workout total reps',
     ],
     'tenancy' => [
         'register_tenant' => 'Register tenant',
