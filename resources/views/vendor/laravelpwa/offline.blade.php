1dfaeaaa77b83174a6d711ba2e9769c512aa11a2
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-09-28 16:42:41 +0200

    RA-33 fixed PWA offline view

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.minimal')
 
-@section('content')
-
+@section('body')
     <h1>You are currently not connected to any networks.</h1>
-
+    <h2 class="mb-3">Repflux needs Internet connection.</h2>
+    <x-button href="/">Retry</x-button>
 @endsection
