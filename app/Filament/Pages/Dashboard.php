a736a8154b95c2fb97a7889bcbc6620d73c8f1e8
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-10-14 08:42:35 +0200

    RA-48: added RecordSet create btn to Dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Pages;
 
+use App\Filament\Resources\RecordSetResource\Pages\CreateRecordSet;
+use Filament\Actions\Action;
+use Filament\Support\Icons\Heroicon;
 use Illuminate\Contracts\Support\Htmlable;
 
 class Dashboard extends \Filament\Pages\Dashboard
@@ -17,5 +20,18 @@ class Dashboard extends \Filament\Pages\Dashboard
     public function getTitle(): string|Htmlable
     {
         return __('pages.dashboard.title');
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('add-set')
+                ->label(__('pages.record_sets.add_set'))
+                ->url(CreateRecordSet::getUrl())
+                ->icon(Heroicon::PlusCircle)
+                ->color('success')
+                ->size('md')
+                ->button(),
+        ];
     }
 }
