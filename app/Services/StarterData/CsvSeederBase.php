415693cf6c64850a4d5b2781c66cf422166df070
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-10-23 18:26:50 +0200

    - RA-37: fixed tests - changed run configurations to use relative paths

--- app/Services/StarterData/CsvSeederBase.php
+++ app/Services/StarterData/CsvSeederBase.php
@@ -3,11 +3,12 @@
 namespace App\Services\StarterData;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 abstract class CsvSeederBase {
     protected static function getCSV(string $filePath): array
     {
-        return Cache::tags(['seeder'])->remember(md5($filePath), 600, function () use ($filePath) {
+        return Cache::tags(['seeder'])->remember(Str::slug($filePath), 600, function () use ($filePath) {
             $types = [];
             $file = fopen(storage_path($filePath), 'r');
             $headers = fgetcsv($file);
--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,200 +1,274 @@
-<?php
-
-return [
-    'anourvalar/eloquent-serialize' => [
-        'aliases' => [
-            'EloquentSerialize' => 'AnourValar\\EloquentSerialize\\Facades\\EloquentSerializeFacade',
-        ],
-    ],
-    'barryvdh/laravel-debugbar' => [
-        'aliases' => [
-            'Debugbar' => 'Barryvdh\\Debugbar\\Facades\\Debugbar',
-        ],
-        'providers' => [
-            0 => 'Barryvdh\\Debugbar\\ServiceProvider',
-        ],
-    ],
-    'barryvdh/laravel-ide-helper' => [
-        'providers' => [
-            0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
-        ],
-    ],
-    'blade-ui-kit/blade-heroicons' => [
-        'providers' => [
-            0 => 'BladeUI\\Heroicons\\BladeHeroiconsServiceProvider',
-        ],
-    ],
-    'blade-ui-kit/blade-icons' => [
-        'providers' => [
-            0 => 'BladeUI\\Icons\\BladeIconsServiceProvider',
-        ],
-    ],
-    'dutchcodingcompany/filament-socialite' => [
-        'aliases' => [
-            'FilamentSocialite' => 'DutchCodingCompany\\FilamentSocialite\\Facades\\FilamentSocialite',
-        ],
-        'providers' => [
-            0 => 'DutchCodingCompany\\FilamentSocialite\\FilamentSocialiteServiceProvider',
-        ],
-    ],
-    'filament/actions' => [
-        'providers' => [
-            0 => 'Filament\\Actions\\ActionsServiceProvider',
-        ],
-    ],
-    'filament/filament' => [
-        'providers' => [
-            0 => 'Filament\\FilamentServiceProvider',
-        ],
-    ],
-    'filament/forms' => [
-        'providers' => [
-            0 => 'Filament\\Forms\\FormsServiceProvider',
-        ],
-    ],
-    'filament/infolists' => [
-        'providers' => [
-            0 => 'Filament\\Infolists\\InfolistsServiceProvider',
-        ],
-    ],
-    'filament/notifications' => [
-        'providers' => [
-            0 => 'Filament\\Notifications\\NotificationsServiceProvider',
-        ],
-    ],
-    'filament/schemas' => [
-        'providers' => [
-            0 => 'Filament\\Schemas\\SchemasServiceProvider',
-        ],
-    ],
-    'filament/support' => [
-        'providers' => [
-            0 => 'Filament\\Support\\SupportServiceProvider',
-        ],
-    ],
-    'filament/tables' => [
-        'providers' => [
-            0 => 'Filament\\Tables\\TablesServiceProvider',
-        ],
-    ],
-    'filament/widgets' => [
-        'providers' => [
-            0 => 'Filament\\Widgets\\WidgetsServiceProvider',
-        ],
-    ],
-    'kirschbaum-development/eloquent-power-joins' => [
-        'providers' => [
-            0 => 'Kirschbaum\\PowerJoins\\PowerJoinsServiceProvider',
-        ],
-    ],
-    'laravel/octane' => [
-        'aliases' => [
-            'Octane' => 'Laravel\\Octane\\Facades\\Octane',
-        ],
-        'providers' => [
-            0 => 'Laravel\\Octane\\OctaneServiceProvider',
-        ],
-    ],
-    'laravel/pail' => [
-        'providers' => [
-            0 => 'Laravel\\Pail\\PailServiceProvider',
-        ],
-    ],
-    'laravel/sail' => [
-        'providers' => [
-            0 => 'Laravel\\Sail\\SailServiceProvider',
-        ],
-    ],
-    'laravel/socialite' => [
-        'aliases' => [
-            'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
-        ],
-        'providers' => [
-            0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
-        ],
-    ],
-    'laravel/tinker' => [
-        'providers' => [
-            0 => 'Laravel\\Tinker\\TinkerServiceProvider',
-        ],
-    ],
-    'larswiegers/laravel-translations-checker' => [
-        'aliases' => [
-            'LaravelTranslationsChecker' => 'Larswiegers\\LaravelTranslationsChecker\\LaravelTranslationsCheckerFacade',
-        ],
-        'providers' => [
-            0 => 'Larswiegers\\LaravelTranslationsChecker\\LaravelTranslationsCheckerServiceProvider',
-        ],
-    ],
-    'leandrocfe/filament-apex-charts' => [
-        'aliases' => [
-            'FilamentApexCharts' => 'Leandrocfe\\FilamentApexCharts\\Facades\\FilamentApexCharts',
-        ],
-        'providers' => [
-            0 => 'Leandrocfe\\FilamentApexCharts\\FilamentApexChartsServiceProvider',
-        ],
-    ],
-    'livewire/livewire' => [
-        'aliases' => [
-            'Livewire' => 'Livewire\\Livewire',
-        ],
-        'providers' => [
-            0 => 'Livewire\\LivewireServiceProvider',
-        ],
-    ],
-    'martinbean/socialite-discord-provider' => [
-        'providers' => [
-            0 => 'MartinBean\\Laravel\\Socialite\\DiscordServiceProvider',
-        ],
-    ],
-    'nesbot/carbon' => [
-        'providers' => [
-            0 => 'Carbon\\Laravel\\ServiceProvider',
-        ],
-    ],
-    'nunomaduro/collision' => [
-        'providers' => [
-            0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
-        ],
-    ],
-    'nunomaduro/termwind' => [
-        'providers' => [
-            0 => 'Termwind\\Laravel\\TermwindServiceProvider',
-        ],
-    ],
-    'owenvoke/blade-fontawesome' => [
-        'providers' => [
-            0 => 'OwenVoke\\BladeFontAwesome\\BladeFontAwesomeServiceProvider',
-        ],
-    ],
-    'ryangjchandler/blade-capture-directive' => [
-        'aliases' => [
-            'BladeCaptureDirective' => 'RyanChandler\\BladeCaptureDirective\\Facades\\BladeCaptureDirective',
-        ],
-        'providers' => [
-            0 => 'RyanChandler\\BladeCaptureDirective\\BladeCaptureDirectiveServiceProvider',
-        ],
-    ],
-    'sentry/sentry-laravel' => [
-        'aliases' => [
-            'Sentry' => 'Sentry\\Laravel\\Facade',
-        ],
-        'providers' => [
-            0 => 'Sentry\\Laravel\\ServiceProvider',
-            1 => 'Sentry\\Laravel\\Tracing\\ServiceProvider',
-        ],
-    ],
-    'silviolleite/laravelpwa' => [
-        'providers' => [
-            0 => 'LaravelPWA\\Providers\\LaravelPWAServiceProvider',
-        ],
-    ],
-    'spatie/laravel-discord-alerts' => [
-        'aliases' => [
-            'Discord' => 'DiscordAlert',
-        ],
-        'providers' => [
-            0 => 'Spatie\\DiscordAlerts\\DiscordAlertsServiceProvider',
-        ],
-    ],
-];
+<?php return array (
+  'anourvalar/eloquent-serialize' => 
+  array (
+    'aliases' => 
+    array (
+      'EloquentSerialize' => 'AnourValar\\EloquentSerialize\\Facades\\EloquentSerializeFacade',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facades\\Debugbar',
+    ),
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-ide-helper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    ),
+  ),
+  'blade-ui-kit/blade-heroicons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BladeUI\\Heroicons\\BladeHeroiconsServiceProvider',
+    ),
+  ),
+  'blade-ui-kit/blade-icons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BladeUI\\Icons\\BladeIconsServiceProvider',
+    ),
+  ),
+  'dutchcodingcompany/filament-socialite' => 
+  array (
+    'aliases' => 
+    array (
+      'FilamentSocialite' => 'DutchCodingCompany\\FilamentSocialite\\Facades\\FilamentSocialite',
+    ),
+    'providers' => 
+    array (
+      0 => 'DutchCodingCompany\\FilamentSocialite\\FilamentSocialiteServiceProvider',
+    ),
+  ),
+  'filament/actions' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Actions\\ActionsServiceProvider',
+    ),
+  ),
+  'filament/filament' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\FilamentServiceProvider',
+    ),
+  ),
+  'filament/forms' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Forms\\FormsServiceProvider',
+    ),
+  ),
+  'filament/infolists' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Infolists\\InfolistsServiceProvider',
+    ),
+  ),
+  'filament/notifications' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Notifications\\NotificationsServiceProvider',
+    ),
+  ),
+  'filament/schemas' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Schemas\\SchemasServiceProvider',
+    ),
+  ),
+  'filament/support' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Support\\SupportServiceProvider',
+    ),
+  ),
+  'filament/tables' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Tables\\TablesServiceProvider',
+    ),
+  ),
+  'filament/widgets' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Widgets\\WidgetsServiceProvider',
+    ),
+  ),
+  'kirschbaum-development/eloquent-power-joins' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Kirschbaum\\PowerJoins\\PowerJoinsServiceProvider',
+    ),
+  ),
+  'laravel/octane' => 
+  array (
+    'aliases' => 
+    array (
+      'Octane' => 'Laravel\\Octane\\Facades\\Octane',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Octane\\OctaneServiceProvider',
+    ),
+  ),
+  'laravel/pail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pail\\PailServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'larswiegers/laravel-translations-checker' => 
+  array (
+    'aliases' => 
+    array (
+      'LaravelTranslationsChecker' => 'Larswiegers\\LaravelTranslationsChecker\\LaravelTranslationsCheckerFacade',
+    ),
+    'providers' => 
+    array (
+      0 => 'Larswiegers\\LaravelTranslationsChecker\\LaravelTranslationsCheckerServiceProvider',
+    ),
+  ),
+  'leandrocfe/filament-apex-charts' => 
+  array (
+    'aliases' => 
+    array (
+      'FilamentApexCharts' => 'Leandrocfe\\FilamentApexCharts\\Facades\\FilamentApexCharts',
+    ),
+    'providers' => 
+    array (
+      0 => 'Leandrocfe\\FilamentApexCharts\\FilamentApexChartsServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+  ),
+  'martinbean/socialite-discord-provider' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MartinBean\\Laravel\\Socialite\\DiscordServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'owenvoke/blade-fontawesome' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'OwenVoke\\BladeFontAwesome\\BladeFontAwesomeServiceProvider',
+    ),
+  ),
+  'ryangjchandler/blade-capture-directive' => 
+  array (
+    'aliases' => 
+    array (
+      'BladeCaptureDirective' => 'RyanChandler\\BladeCaptureDirective\\Facades\\BladeCaptureDirective',
+    ),
+    'providers' => 
+    array (
+      0 => 'RyanChandler\\BladeCaptureDirective\\BladeCaptureDirectiveServiceProvider',
+    ),
+  ),
+  'sentry/sentry-laravel' => 
+  array (
+    'aliases' => 
+    array (
+      'Sentry' => 'Sentry\\Laravel\\Facade',
+    ),
+    'providers' => 
+    array (
+      0 => 'Sentry\\Laravel\\ServiceProvider',
+      1 => 'Sentry\\Laravel\\Tracing\\ServiceProvider',
+    ),
+  ),
+  'silviolleite/laravelpwa' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'LaravelPWA\\Providers\\LaravelPWAServiceProvider',
+    ),
+  ),
+  'spatie/laravel-discord-alerts' => 
+  array (
+    'aliases' => 
+    array (
+      'Discord' => 'DiscordAlert',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\DiscordAlerts\\DiscordAlertsServiceProvider',
+    ),
+  ),
+);
