ffc16544957813bce303e35b165a234ea705e7c1
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-10-31 08:39:19 +0100

    pint

--- app/Filament/Columns/ShortDateColumn.php
+++ app/Filament/Columns/ShortDateColumn.php
@@ -4,7 +4,8 @@ namespace App\Filament\Columns;
 
 use Filament\Tables\Columns\TextColumn;
 
-class ShortDateColumn extends TextColumn {
+class ShortDateColumn extends TextColumn
+{
     public static function make(?string $name = null): static
     {
         return parent::make($name)
