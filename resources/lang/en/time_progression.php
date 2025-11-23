1644989fa6db63b8170868c1e7a30d834e0547b3
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-11-23 22:06:26 +0100

    RA-93: add missing translations

---
+++ resources/lang/en/time_progression.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'up' => [
+        'label' => 'Up',
+        'description' => 'The longer, the better.',
+    ],
+    'down' => [
+        'label' => 'Down',
+        'description' => 'The shorter, the better.',
+    ],
+];
