92c3b41c4b1441edba7e97a468a9f560e34c0c95
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2026-05-11 22:01:08 +0200

    RA-130: auto refresh maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,4 +4,23 @@
 
 @section('content')
     <p>We are under maintenance. Please come back later!</p>
+    <p class="text-gray-400 animate-pulse mt-5" id="msg">Retrying...</p>
+
+    <script>
+        let maintenanceInterval = null;
+
+        window.onload = function () {
+            maintenanceInterval = setInterval(testServer, 5000);
+        };
+
+        function testServer() {
+            fetch('/app').then((result) => {
+                if (result.status === 200) {
+                    document.getElementById('msg').textContent = 'We are back!';
+                    clearInterval(maintenanceInterval);
+                    window.location.reload();
+                }
+            })
+        }
+    </script>
 @endsection
